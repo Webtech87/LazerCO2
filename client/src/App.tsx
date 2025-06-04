c12fefcf1d20ca6e5b685c1e73b486c9551e6eb7
robertowebtech87 <roberto.santiago@webtech87.pt>
2025-06-04 07:02:07 +0100

    remove before/after section

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import { useEffect } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import { HeroSection } from './components/Hero/HeroSection';
 import About from "./components/About/About";
-import BeforeAfter from "./components/BeforeAfter/BeforeAfter";
 import './App.css';
 import Benefits from './components/Benefits/Benefits';
 
@@ -28,27 +27,10 @@ const App = () => {
   return (
     <div className="App">
       <Navbar />
-      
       <HeroSection className="hero-section" />
       <About />
       <Benefits />
-      <BeforeAfter
-        imageSources={[
-          'https://cdn.builder.io/api/v1/image/assets/TEMP/0af60b8fc47314140fc250b53bced117aadb90df?placeholderIfAbsent=true',
-          
-        ]}
-        title="Laser CO₂ Aplicado com Precisão nas Zonas-Chave"
-        subtitle="Rosto, pescoço e colo"
-        benefits={[
-          "Ideal para melhorar:",
 
-
-          "Textura e firmeza da pele",
-          "Rugas finas e moderadas",
-          "Manchas solares e sinais de envelhecimento",
-          "Flacidez leve Permite um efeito lifting natural, com renovação visível da pele após cada sessão.",
-        ]}
-      />
     </div>
   );
 };
